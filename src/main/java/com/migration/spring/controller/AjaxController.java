976a30c224be3f469f147ddab006e22a15f457b3
Prasoops <prasoops@gmail.com>
2017-01-26 15:57:32 +0530

    first commit

--- src/main/java/com/migration/spring/controller/AjaxController.java
+++ src/main/java/com/migration/spring/controller/AjaxController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AjaxController {
+	//Prasad
 
 	@RequestMapping(value = "/conditions", method = RequestMethod.GET)
 	public @ResponseBody Map<String,String> conditions(Model model,@RequestParam String circuitType) {
